7acc0e3795c41f8714c64352e9861bdd05189c00
Vidhu Panhavoor Vasudevan <vidhuvasudevan.2000@gmail.com>
2026-02-10 01:52:26 -0800

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,14 +1,16 @@
+import os
 import sys
 
 
 def main():
-    # TODO: Uncomment the code below to pass the first stage
 
     while True:
         sys.stdout.write("$ ")
         command = input()
         builtin = ["exit", "echo", "type"]
         words = command.split()
+        command_strip = command[5:].strip()
+        path_string = os.getenv("PATH")
         if command == "exit":
             break
         elif words[0] == "echo":
@@ -17,6 +19,15 @@ def main():
         elif words[0] == "type":
             if words[1] in builtin:
                 print(f"{words[1]} is a shell builtin")
+            elif path_string:
+                paths = path_string.split(os.pathsep)
+                found = False
+                for path in paths:
+                    if os.access(path + f"/{command_strip}", os.X_OK):
+                        print(f"{command_strip} is {path}/{command_strip}")
+                    else:
+                        break
+
             else:
                 print(f"{words[1]}: not found")
         else:
